15cc46ed1a8bba1ca516b6f3d0b3de7fd6fc2225
blydiet <lydietoussaint123@gmail.com>
2019-07-26 14:22:59 -0400

    (feature-Added-Window)-I added a popup window so that a user can watch videos and get them translated if no caption on the scrren

---
+++ app.js
@@ -0,0 +1,12 @@
+let translateButton = document.getElementById('button');
+
+if (translateButton) {
+  translateButton.onclick = () => {
+    window.open(
+      '',
+      '_blank',
+      'toolbar=yes,scrollbars=yes,resizable=yes,width=400,height=400'
+    );
+    translateButton.disabled = true;
+  };
+}
