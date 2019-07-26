4deebfa2d7be13d696d597bcf5d80baafef4d4f4
blydiet <lydietoussaint123@gmail.com>
2019-07-26 14:39:04 -0400

    (feature-Add-Translate)-I imported the other part of my translate application so now a user can translate speech

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let translateButton = document.getElementById('button');
 if (translateButton) {
   translateButton.onclick = () => {
     window.open(
-      '',
+      'https://blydiet.github.io/translate/',
       '_blank',
       'toolbar=yes,scrollbars=yes,resizable=yes,width=400,height=400'
     );
